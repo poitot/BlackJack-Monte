7bb605884f99e190e9b173aaddd7c77e741382ef
Connor <connortennant97@gmail.com>
2020-03-13 06:36:38 +0000

    21 + 3 side bet

--- blackjack_env2/__init__.py
+++
@@ -1,6 +0,0 @@
-from gym.envs.registration import register
-
-register(
-    id='blackjack-v2', 
-    entry_point='blackjack_env2.env.blackjack_env2:BlackjackEnv',
-)
--- blackjack_env2/env/__init__.py
+++
@@ -1,1 +0,0 @@
-import blackjack_env2
